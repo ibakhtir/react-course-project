85cba5185a1b8909fd7b6e44d0f54d3e4a697f6b
ibakhtir <ibakhtir@gmail.com>
2022-07-01 18:33:39 +0300

    before hw-1

--- src/app/hooks/useUsers.jsx
+++ src/app/hooks/useUsers.jsx
@@ -7,60 +7,62 @@ import { useAuth } from "./useAuth";
 const UserContext = React.createContext();
 
 export const useUser = () => {
-  return useContext(UserContext);
+    return useContext(UserContext);
 };
 
 const UserProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const { currentUser } = useAuth();
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    getUsers();
-  }, []);
-  useEffect(() => {
-    if (error !== null) {
-      toast(error);
-      setError(null);
+    const [users, setUsers] = useState([]);
+    const { currentUser } = useAuth();
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        getUsers();
+    }, []);
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+    async function getUsers() {
+        try {
+            const { content } = await userService.get();
+            setUsers(content);
+            setLoading(false);
+        } catch (error) {
+            errorCatcher(error);
+        }
     }
-  }, [error]);
-  async function getUsers() {
-    try {
-      const { content } = await userService.get();
-      setUsers(content);
-      setLoading(false);
-    } catch (error) {
-      errorCatcher(error);
+    useEffect(() => {
+        if (!isLoading) {
+            const newUsers = [...users];
+            const indexUser = newUsers.findIndex(
+                (u) => u._id === currentUser._id
+            );
+            newUsers[indexUser] = currentUser;
+            setUsers(newUsers);
+        }
+    }, [currentUser]);
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+        setLoading(false);
     }
-  }
-  useEffect(() => {
-    if (!isLoading) {
-      const newUsers = [...users];
-      const indexUser = newUsers.findIndex((u) => u._id === currentUser._id);
-      newUsers[indexUser] = currentUser;
-      setUsers(newUsers);
+    function getUserById(userId) {
+        return users.find((u) => u._id === userId);
     }
-  }, [currentUser]);
-  function errorCatcher(error) {
-    const { message } = error.response.data;
-    setError(message);
-    setLoading(false);
-  }
-  function getUserById(userId) {
-    return users.find((u) => u._id === userId);
-  }
-  return (
-    <UserContext.Provider value={{ users, getUserById }}>
-      {!isLoading ? children : "Loading..."}
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={{ users, getUserById }}>
+            {!isLoading ? children : "Loading..."}
+        </UserContext.Provider>
+    );
 };
 
 UserProvider.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
 };
 
 export default UserProvider;
--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -6,28 +6,28 @@ import UsersListPage from "../components/page/usersListPage";
 import { useAuth } from "../hooks/useAuth";
 import UserProvider from "../hooks/useUsers";
 const Users = () => {
-  const params = useParams();
-  const { userId, edit } = params;
-  const { currentUser } = useAuth();
-  return (
-    <>
-      <UserProvider>
-        {userId ? (
-          edit ? (
-            userId === currentUser._id ? (
-              <EditUserPage />
-            ) : (
-              <Redirect to={`/users/${currentUser._id}/edit`} />
-            )
-          ) : (
-            <UserPage userId={userId} />
-          )
-        ) : (
-          <UsersListPage />
-        )}
-      </UserProvider>
-    </>
-  );
+    const params = useParams();
+    const { userId, edit } = params;
+    const { currentUser } = useAuth();
+    return (
+        <>
+            <UserProvider>
+                {userId ? (
+                    edit ? (
+                        userId === currentUser._id ? (
+                            <EditUserPage />
+                        ) : (
+                            <Redirect to={`/users/${currentUser._id}/edit`} />
+                        )
+                    ) : (
+                        <UserPage userId={userId} />
+                    )
+                ) : (
+                    <UsersListPage />
+                )}
+            </UserProvider>
+        </>
+    );
 };
 
 export default Users;
